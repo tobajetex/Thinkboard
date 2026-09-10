a5c8ff532cecc9bc10d5fc35e34a1f6e7ea7a189
tobajetex <jetawotobajetex@gmail.com>
2026-09-10 14:19:31 +0100

    feat: add env for local linking the frontend to backend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+const BASE_URL = (import.meta.env.MODE = import.meta.env.VITE_API_URL);
 
 const api = axios.create({
   baseURL: BASE_URL,
